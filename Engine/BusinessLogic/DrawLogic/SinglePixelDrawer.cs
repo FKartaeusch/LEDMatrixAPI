a7adaa664ff7ef98ab1a32b67341ad35375bebab
Frank <f.kartaeusch@web.de>
2020-04-14 19:27:05 +0200

    thread sleep after update

--- Engine/BusinessLogic/DrawLogic/SinglePixelDrawer.cs
+++ Engine/BusinessLogic/DrawLogic/SinglePixelDrawer.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Device.Spi;
+using System.Threading;
 using Engine.Core.Connection;
 using Engine.DeviceUtils.LEDMatrixWS2812B;
 using Engine.Models.DTO;
 using IconsAndFonts.Icons;
-using Iot.Device.Ws28xx;
 
 namespace Engine.BusinessLogic.DrawLogic
 {
@@ -21,19 +20,13 @@ namespace Engine.BusinessLogic.DrawLogic
 
         public void Draw(SinglePixelDTO pixelDTO)
         {
-            var count = 256; // number of LEDs
-            var settings = new SpiConnectionSettings(0, 0)
-            {
-                ClockFrequency = 2_400_000, Mode = SpiMode.Mode0, DataBitLength = 8
-            };
+            var device = _connector.GetDevice();
 
-            var spi = SpiDevice.Create(settings);
-            var device = new Ws2812b(spi, count);
-            //var device = _connector.GetDevice();
             var pixelAdress = _pixelPointer.GetDevicePixel(pixelDTO.PixelLocation);
             Console.WriteLine("Adressing Pixel " + pixelAdress);
             device.Image.SetPixel(pixelAdress, 0, pixelDTO.Color);
             device.Update();
+            Thread.Sleep(100);
         }
 
         public void Draw(PixelInformation pixel)
