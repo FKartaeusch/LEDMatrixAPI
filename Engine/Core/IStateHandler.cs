65bb847eb8a4dc2340a7220442d07072db44357d
Frank <f.kartaeusch@web.de>
2020-04-14 11:09:12 +0200

    added missing stateinterface

---
+++ Engine/Core/IStateHandler.cs
@@ -0,0 +1,14 @@
+﻿using Engine.Models;
+using Engine.Models.DTO;
+
+namespace Engine.Core
+{
+    public interface IStateHandler
+    {
+        public State GetCurrentState();
+
+        public void SetState(StateDTO stateDTO);
+
+        public void ChangeState(StateDTO dto);
+    }
+}
