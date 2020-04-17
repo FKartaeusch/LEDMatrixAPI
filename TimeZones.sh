751e297eaaeeabf49523f791ddbeecfa741c882e
Frank <f.kartaeusch@web.de>
2020-04-17 14:40:01 +0200

    remove sh file

--- TimeZones.sh
+++
@@ -1,7 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-#install tzdata package
-sudo apt-get update
-sudo apt-get install tzdata -y
-
-sudo ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
-sudo dpkg-reconfigure --frontend noninteractive tzdata
